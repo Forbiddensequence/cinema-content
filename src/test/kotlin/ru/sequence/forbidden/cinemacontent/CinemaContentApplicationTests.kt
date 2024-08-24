3151ee46c887c2d55a596e697baf37ba3aa5e6a6
Kornilov Alexandr <targaryen1337@yandex.ru>
2024-08-24 16:21:48 +0300

    Init commit with additional files :_)

---
+++ src/test/kotlin/ru/sequence/forbidden/cinemacontent/CinemaContentApplicationTests.kt
@@ -0,0 +1,15 @@
+package ru.sequence.forbidden.cinemacontent
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.annotation.Import
+
+@Import(TestcontainersConfiguration::class)
+@SpringBootTest
+class CinemaContentApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
