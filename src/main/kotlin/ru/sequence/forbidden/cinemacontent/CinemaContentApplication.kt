80185c348aee35abdd38b8e3fef01271c659deda
Kornilov Alexandr <targaryen1337@yandex.ru>
2024-08-24 16:21:48 +0300

    Init commit with additional files :_)

---
+++ src/main/kotlin/ru/sequence/forbidden/cinemacontent/CinemaContentApplication.kt
@@ -0,0 +1,11 @@
+package ru.sequence.forbidden.cinemacontent
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CinemaContentApplication
+
+fun main(args: Array<String>) {
+    runApplication<CinemaContentApplication>(*args)
+}
